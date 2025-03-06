9b80c583b43fe8e9d39f3646ad70228f4817a2cc
mafna <mafna@AUTO9QO20TJP5MU>
2025-03-06 11:05:52 +0530

    updated header toggle button logic

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { KENDO_BUTTONS } from '@progress/kendo-angular-buttons';
-import { NavigationEnd, Router, RouterLink } from '@angular/router';
-
-
-import { filter, Subscription } from 'rxjs';
+import { Router, RouterLink } from '@angular/router';
 
 
 @Component({
@@ -16,32 +13,10 @@ import { filter, Subscription } from 'rxjs';
 })
 export class HeaderComponent {
 
-
-  activeRoute: string = 'register'; 
-  private routeSubscription: Subscription = new Subscription();
-
   constructor(private router: Router) {}
   
-  ngOnInit(): void {
-    const currentUrl = this.router.url.split('/').pop() || 'register';
-    this.activeRoute = currentUrl;
-    
-    this.routeSubscription = this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe((event: any) => {
-        const url = event.urlAfterRedirects || event.url;
-        this.activeRoute = url.split('/').pop() || 'register';
-      });
-  }
-  
-  ngOnDestroy(): void {
-    if (this.routeSubscription) {
-      this.routeSubscription.unsubscribe();
-    }
-  }
-  
   isRouteActive(route: string): boolean {
-    return this.activeRoute === route;
+    return route == this.router.url.split('/').pop();
   }
 
 }
